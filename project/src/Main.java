64840df67aaad66b399e2ae05846fbe5468b0b9a
Kalin Dobrev <dobrevkalm@gmail.com>
2020-03-25 21:16:05 +0100

    Added testing class

--- project/src/Main.java
+++ project/src/Main.java
@@ -2,27 +2,32 @@ import helpers.*;
 import model.PathResult;
 import model.Place;
 import reader.DataReader;
+import test.HeuristicTest;
 
 import java.util.List;
 
 public class Main {
+    private static final int ALL_PROFITS = 380;
     private static DataReader reader = new DataReader();
+    private static double[][] distanceMatrix = reader.getDistanceMatrix();
+    private static Place[] places = reader.getAllCompanies();
 
     public static void main(String[] args) {
-        double[][] distanceMatrix = reader.getDistanceMatrix();
-        Place[] places = reader.getAllCompanies();
-        int startVertex = 5;
-        int agentsNumber = 2;
-        // in billions MAX is 380.70
-        int minProfit = 200;
+        runTests();
+    }
 
-        Heuristic hOne = new HeuristicOne(distanceMatrix, places, startVertex, agentsNumber, minProfit);
-        HeuristicTwo hTwo = new HeuristicTwo(distanceMatrix, places, startVertex, agentsNumber, minProfit);
-
-        // Results for solution 1
-        printResults(hOne.getClass().getName(), hOne.getResultPaths());
-        // Results for solution 2
-        printResults(hTwo.getClass().getName(), hTwo.getResultPaths());
+    private static void getResultPath(boolean one, int startVertex, int agentsNumber, int minProfit) {
+        if (minProfit > (ALL_PROFITS - places[startVertex].getFirmProfit())) {
+            System.out.println("Not enough profit to collect");
+        } else {
+            Heuristic h;
+            if (one) {
+                h = new HeuristicOne(distanceMatrix, places, startVertex, agentsNumber, minProfit);
+            } else {
+                h = new HeuristicTwo(distanceMatrix, places, startVertex, agentsNumber, minProfit);
+            }
+            printResults(h.getClass().getName(), h.getResultPaths());
+        }
     }
 
     private static void printResults(String method, PathResult[] results) {
@@ -32,15 +37,21 @@ public class Main {
             System.out.println("\n Result for agent #" + agent++);
             printResultPath(pr.getResultPath());
         }
-        System.out.println("\nTotal length: " + getTotalLength(results));
-        System.out.println(getTotalProfitCollected(results));
+        printResultFooter(getTotalLength(results), getTotalProfitCollected(results));
+    }
+
+    private static void printResultFooter(double length, double profit) {
+        System.out.printf("%n%s %.2f", "Total length:", length);
+        System.out.printf("%n%s %.2f%n", "Total profit:", profit);
+        System.out.println("\n########################");
+        System.out.println("########################\n\n");
     }
 
     private static void printResultHeader(String method) {
         String name = method.substring(method.indexOf('.') + 1);
-        System.out.println("\n ######################## ");
-        System.out.println(" > " + name + " Results <");
-        System.out.println(" ######################## ");
+        System.out.println("\n########################");
+        System.out.println("> " + name + " Results <");
+        System.out.println("########################");
     }
 
     private static double getTotalLength(PathResult[] results) {
@@ -61,5 +72,10 @@ public class Main {
 
     private static void printResultPath(List<Place> places) {
         System.out.println(places.toString().replaceAll("\\[", "Start").replaceAll("\\]", "\nEnd"));
+    }
+
+    private static void runTests() {
+        HeuristicTest ht = new HeuristicTest();
+        ht.runHeuristicTests();
     }
 }
---
+++ project/src/test/HeuristicTest.java
@@ -0,0 +1,44 @@
+package test;
+
+import helpers.*;
+import model.*;
+import reader.*;
+
+public class HeuristicTest {
+    private DataReader reader = new DataReader();
+
+    public void runHeuristicTests() {
+        double[][] matrix = reader.getDistanceMatrix();
+        Place[] places = reader.getAllCompanies();
+        int vertex = 0;
+        int agents = 1;
+        int profit = 100;
+
+        final int P = 380;
+        final int V = places.length;
+        // we have 91 companies so we should not need more than 10 agents...
+        final int A = 10;
+
+        while (vertex < V) {
+            final int MAX_P = (int) (P - places[vertex].getFirmProfit());
+            while (agents < A) {
+                while(profit < MAX_P) {
+                    Heuristic h1 = new HeuristicOne(matrix, places, vertex, agents, profit);
+                    Heuristic h2 = new HeuristicOne(matrix, places, vertex, agents, profit);
+                    System.out.printf("%n| V = %d | A = %d | P = %d |%n", vertex, agents, profit);
+                    h1.getResultPaths();
+                    System.out.println("------------------------");
+                    System.out.println("--- ### ONE DONE ### ---");
+                    h2.getResultPaths();
+                    System.out.println("--- ### TWO DONE ### ---");
+                    System.out.println("------------------------");
+                    profit += 5;
+                }
+                profit = 100;
+                agents++;
+            }
+            agents = 1;
+            vertex++;
+        }
+    }
+}
