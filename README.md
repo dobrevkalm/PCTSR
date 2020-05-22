42323c8f4feefec7e8450f9060fe452c7dd017e4
Kalin Dobrev <dobrevkalm@gmail.com>
2020-05-22 23:46:44 +0200

    Update the main method (pretify the execution process)

--- README.md
+++ README.md
@@ -12,17 +12,21 @@ The companies location was gathered using Pyhon 3 and GeoPy (https://geopy.readt
 The simple grapgic user interface allows the user to enter starting vertex (the company they wish to start from), desired profit (the profit to be collected), number of agents (each agent will have an unique route) and choose between two possible solutions (one or two, representing two different algorithms).
 
 ## How to run
- - javac *.java - to compile all the classes
+First compile all the classes:
+ - javac *.java
+
+Then choose what to run:
  - java Main - run the GUI
  - java Main -t - run tests
  - java Main -rp - compute a tour and output it on the console (without the GUI)
  - java Main -e - run all the experiments (results are saved to .csv files)
- - java Main -e {1, 2, 3, 4, 5} - run a specific experiment
-    = 1 - HeuristicOne coefficient
-    = 2 - kmax parameter for HeuristicTwo
-    = 3 - route modification percent for HeuristicTwo
-    = 4 - mutations ratio for HeuristicTwo
-    = 5 - comparisson between all heuristic methods
+
+To run a specific experiment, add a number between 1 and 5:
+ - java Main -e 1 - run experiment for HeuristicOne coefficient
+ - java Main -e 2 - kmax parameter for HeuristicTwo
+ - java Main -e 3 - route modification percent for HeuristicTwo
+ - java Main -e 4 - mutations ratio for HeuristicTwo
+ - java Main -e 5 - comparisson between all heuristic methods
 
 ## Authors
 Adrianna Wiacek (https://github.com/losica) & Kalin Dobrev (https://github.com/dobrevkalm)
--- project/src/Main.java
+++ project/src/Main.java
@@ -2,9 +2,35 @@ import GUI.GUI;
 import experiments.*;
 import test.*;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        runGUI();
+        if (args.length == 0) {
+            runGUI();
+        } else {
+            switch (args[0]) {
+                case "-t":
+                    runGeneralTests();
+                    break;
+                case "-rp":
+                    runResultPathTest();
+                    break;
+                case "-e": {
+                    if (args.length == 1) {
+                        runAllExperiments();
+                    } else {
+                        runExperiment(Integer.parseInt(args[1]));
+                    }
+                    break;
+                }
+                default:
+                    wrongArgument();
+                    System.exit(0);
+            }
+        }
     }
 
     // runs the interface
@@ -13,19 +39,78 @@ public class Main {
         gui.run();
     }
 
-    private static void runExperiment() {
-        Experiment e = new HeuristicOneExperiment("name_me.csv");
+    private static void runAllExperiments() {
+        Experiment[] experiments = new Experiment[] {
+                new HeuristicOneExperiment("heuristic_one.csv"),
+                new H2kmaxExperiment("heuristic_two_kmax.csv"),
+                new H2percentExperiment("heuristic_two_percent.csv"),
+                new H2ratioExperiment("heuristic_two_ratio.csv"),
+                new HeuristicComparison("heuristic_comparison.csv")
+        };
+
+        for (Experiment e : experiments) {
+            e.run();
+        }
+    }
+
+    private static void runExperiment(int experiment) {
+        String timeStamp = new SimpleDateFormat("dd-MM-yyyy HH.mm.ss").format(new Date());
+        String fileName;
+        Experiment e = null;
+
+        switch (experiment) {
+            case 1: {
+                fileName = "h_one_" + timeStamp + ".csv";
+                e = new HeuristicOneExperiment(fileName);
+                break;
+            }
+            case 2: {
+                fileName = "h_two_kmax_" + timeStamp + ".csv";
+                e = new H2kmaxExperiment(fileName);
+                break;
+            }
+            case 3: {
+                fileName = "h_two_percent_" + timeStamp + ".csv";
+                e = new H2percentExperiment(fileName);
+                break;
+            }
+            case 4: {
+                fileName = "h_two_ratio_" + timeStamp + ".csv";
+                e = new H2ratioExperiment(fileName);
+                break;
+            }
+            case 5: {
+                fileName = "h_comparison_" + timeStamp + ".csv";
+                e = new HeuristicComparison(fileName);
+                break;
+            }
+            default:
+                System.out.println("Wrong experiment selected!\nAvailable: [1, 2, 3, 4, 5]");
+                System.exit(0);
+        }
+
         e.run();
     }
 
-    /**
-     * Run this method to get a console output of the result path calculated
-     * @param heuristic the method you want to test - [one, two, three]
-     * @param startVertex the company we want to start at (0 to 90)
-     * @param agentsNumber the number of agents collecting profit (1 to 10)
-     * @param minProfit the desired collected profit
-     */
-    private static void runResultPathTest(String heuristic, int startVertex, int agentsNumber, int minProfit) {
+    private static void runResultPathTest() {
+        Scanner sc = new Scanner(System.in);
+
+        // select heuristic method
+        System.out.println(" - Choose heuristic method.\n - Enter one, two, three or four:");
+        String heuristic = sc.nextLine();
+
+        // select the starting vertex
+        System.out.println(" - Choose starting vertex.\n - Enter a number between 0 and 90:");
+        int startVertex = Integer.parseInt(sc.nextLine());
+
+        // select the number of agents
+        System.out.println(" - Choose the number of sales representatives.\n - Enter a number between 1 and 10:");
+        int agentsNumber = Integer.parseInt(sc.nextLine());
+
+        // select desired profit
+        System.out.println(" - Choose desired minimum collected profit.\n - Enter value between 5 and 299:");
+        int minProfit = Integer.parseInt(sc.nextLine());
+
         ResultPathTest rpt = new ResultPathTest();
         rpt.getResultPath(heuristic, startVertex, agentsNumber, minProfit);
     }
@@ -36,5 +121,11 @@ public class Main {
         ht.runTests();
         ReaderTest rt = new ReaderTest();
         rt.runTests();
+    }
+
+    private static void wrongArgument() {
+        System.out.println("## Wrong argument entered ##");
+        System.out.println("## Available arguments: [-t, -rp, -e, -e {number} ##");
+        System.out.println("## Visit https://github.com/dobrevkalm/PCTSR for more info ##");
     }
 }
