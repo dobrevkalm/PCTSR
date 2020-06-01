a0317d05ad51c29fe4b7c10e10059e3a1dea4242
Kalin Dobrev <dobrevkalm@gmail.com>
2020-06-01 11:35:46 +0200

    Improved Heuristic tests. Added assertions and more checkings

--- project/src/test/HeuristicTest.java
+++ project/src/test/HeuristicTest.java
@@ -12,7 +12,7 @@ public class HeuristicTest {
         Place[] places = reader.getAllCompanies();
         int vertex = 0;
         int agents = 1;
-        int profit = 100;
+        int profit = 10;
 
         double P = 0d;
         for (Place place : places) {
@@ -35,15 +35,40 @@ public class HeuristicTest {
                     };
 
                     for (Heuristic h : H) {
-                        h.getResultPaths();
+                        PathResult[] pr = h.getResultPaths();
+                        double totalProfit = getTotalProfit(pr);
+                        assert totalProfit >= profit;
+                        if (profit >= 290) {
+                            evaluatePathResults(pr, !h.getMethodName().equals("HeuristicThree"));
+                        }
                     }
-                    profit += 5;
+                    profit += (profit < 250) ? 10 : 5;
                 }
-                profit = 100;
+                profit = 10;
                 agents++;
             }
             agents = 1;
             vertex++;
         }
+    }
+
+    private void evaluatePathResults(PathResult[] pr, boolean notH3) {
+        for (PathResult p : pr) {
+            if (notH3) {
+                // if the solutions is not HeuristicThree, each agent should be utilized and should bring some profit
+                assert p.getResultPath().size() > 2 && p.getActualProfit() > 0;
+            } else {
+                // in HeuristicThree, if an agent is not utilized, his/her profit should be 0 and visited places should be 2 (just the starting vertex added twice)
+                assert (p.getResultPath().size() > 2 && p.getActualProfit() > 0) || (p.getActualProfit() == 0 && p.getResultPath().size() == 2);
+            }
+        }
+    }
+
+    private double getTotalProfit(PathResult[] pr) {
+        double res = 0d;
+        for (PathResult p : pr) {
+            res += p.getActualProfit();
+        }
+        return res;
     }
 }
